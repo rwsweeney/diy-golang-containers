75191eb67324d26066f23d9538e01232eca6b28d
rwsweeney <rachelwsweeney@gmail.com>
2022-05-24 21:09:41 -0400

    Slight tweaks for more info

--- main.go
+++ main.go
@@ -20,32 +20,27 @@ func main() {
 
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	
+
 	// This holds the cloneflags which  will be passed to the clone syscall that gets called during cmd.Run()
 	// The clone syscall creates a new process
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
+	must(cmd.Run())
 }
 
 func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
 }
 
 func must(err error) {
